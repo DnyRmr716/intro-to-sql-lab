b453a07d3ccc0f23aece656c141187340300d885
DannyR716 <01dromero10@gmail.com>
2024-10-30 14:16:02 -0400

    finished lab

--- clues.sql
+++ clues.sql
@@ -1,26 +1,84 @@
+\connect world
+
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
- 
+
 -- Write SQL query here
+SELECT name
+FROM countries
+WHERE region = 'Southern Europe'
+ORDER BY population ASC
+LIMIT 1;
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
+SELECT language
+FROM countrylanguages
+WHERE countrycode = (
+    SELECT code
+    FROM countries
+    WHERE region = 'Southern Europe'
+    ORDER BY population ASC
+    LIMIT 1
+)
+AND isofficial = TRUE;
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
+SELECT name
+FROM countries
+WHERE code IN (
+    SELECT countrycode
+    FROM countrylanguages
+    WHERE language = 'Italian'
+    AND countrycode NOT IN (
+        SELECT countrycode
+        FROM countrylanguages
+        WHERE language != 'Italian'
+    )
+);
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
+SELECT ci.name
+FROM cities ci
+JOIN countries co ON ci.countrycode = co.code
+WHERE co.code IN (
+    SELECT countrycode
+    FROM countrylanguages
+    WHERE language = 'Italian'
+    AND countrycode NOT IN (
+        SELECT countrycode
+        FROM countrylanguages
+        WHERE language != 'Italian'
+    )
+)
+AND ci.name != co.name
+AND NOT EXISTS (
+    SELECT 1
+    FROM cities other_cities
+    WHERE other_cities.countrycode = ci.countrycode
+    AND other_cities.name = co.name
+);
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+SELECT ci.name AS city_name, co.name AS country_name
+FROM cities ci
+JOIN countries co ON ci.countrycode = co.code
+WHERE ci.name ILIKE '%Vat%'
+  AND ci.name NOT ILIKE '%cano'
+  AND co.continent = 'South America';
+
+
+
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -28,6 +86,12 @@
 -- follow right behind you!
 
 -- Write SQL query here
+
+SELECT ci.name AS capital_city
+FROM cities ci
+JOIN countries co ON ci.id = co.capital
+WHERE co.name = 'Brazil';
+
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -42,3 +106,8 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT name, countrycode
+FROM cities
+WHERE population = 91084;
+
